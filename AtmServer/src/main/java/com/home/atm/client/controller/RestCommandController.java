36050e5ea9f8b743bd08972f4d1eaf9c111525fa
alexandr <alexandr.makarevich88@gmail.com>
2017-07-05 17:47:55 +0300

    added in AccountDao method addAccount added RestControllers(currency,account,command) checked that the controllers worked correctly added maven dependency for json

---
+++ AtmServer/src/main/java/com/home/atm/client/controller/RestCommandController.java
@@ -0,0 +1,29 @@
+package com.home.atm.client.controller;
+
+import com.home.atm.client.CommandBean;
+import com.home.atm.command.Command;
+import com.home.atm.command.PrintBalance;
+import com.home.atm.command.parser_command.DelegatedInputParser;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class RestCommandController {
+
+    private DelegatedInputParser delegatedInputParser;
+
+    @RequestMapping(value = "/commandTest", method = RequestMethod.POST)
+    public List<PrintBalance> processCommand(@RequestBody CommandBean commandBean) {
+        Command commandInt = delegatedInputParser.defaultParseInput(commandBean.getCommandName());
+        return commandInt.executeDb(commandBean.getAccountId());
+    }
+
+    @Resource(name = "delegatedInputParser")
+    public void setDelegatedInputParser(DelegatedInputParser delegatedInputParser) {
+        this.delegatedInputParser = delegatedInputParser;
+    }
+}
